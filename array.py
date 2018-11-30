c44d1aa6f0f2df39ac720d0f41e2d8ef9431b46a
asd29 <45414334+asd29@users.noreply.github.com>
2018-11-30 19:51:34 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,15 @@
+
+odd=[]
+even=[]
+for num in range(100):
+        print(num)
+        if num%2==0:
+                even.append(num)
+                print(even)
+        else:
+                odd.append(num)
+                print(odd)
+        
+
+
+
